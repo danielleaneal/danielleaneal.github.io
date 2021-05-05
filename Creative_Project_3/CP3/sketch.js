0904c19a6c088bb3a42678259075d033e9a1e165
danielleaneal <54646998+danielleaneal@users.noreply.github.com>
2021-05-05 14:15:57 -0400

    Updating Sketch.js
    Including my own changes

--- Creative_Project_3/CP3/sketch.js
+++ Creative_Project_3/CP3/sketch.js
@@ -1,25 +1,39 @@
 /*
- * @name Song
+ * @name Synesthesia: Play the Rainbow, by Danielle Neal
  * @frame 720, 430
- * @description Play a song.
+ * @description Play a song, play the rainbow. Explore how
+ playing the colors versus playing the music impacts the music
+ I wanted to allow the music to take on a synesthetic experience
+ in this sketch, blurring the lines between music and art. I 
+ incorporated random colors, mixed up notes to add silliness,
+ and allowed notes' duration to be longer to allow users to 
+ create longer songs that have visuals along with them. I also 
+ made the colors flicker for as long as the mouse is pressed
+ so that the visuals could be as much a part of the "song" as the 
+ musical tones are.
+ This Project was created using p5js example "Hello_p5: song."
+ 
  * You will need to include the
  * <a href="http://p5js.org/reference/#/libraries/p5.sound">p5.sound
  * library</a> for this example to work in your own project.
  */
 // The midi notes of a scale
-let notes = [ 60, 62, 64, 65, 67, 69, 71];
+ 
 
-// For automatically playing the song
+
+let notes = [ 60, 65, 70, 75, 80, 85, 71];
+
+
 let index = 0;
 let song = [
-  { note: 4, duration: 400, display: "D" },
-  { note: 0, duration: 200, display: "G" },
-  { note: 1, duration: 200, display: "A" },
-  { note: 2, duration: 200, display: "B" },
-  { note: 3, duration: 200, display: "C" },
-  { note: 4, duration: 400, display: "D" },
-  { note: 0, duration: 400, display: "G" },
-  { note: 0, duration: 400, display: "G" }
+  { note: 4, duration: 1000, display: "a" },
+  { note: 0, duration: 1000, display: "c" },
+  { note: 1, duration: 1000, display: "A" },
+  { note: 2, duration: 1000, display: "B" },
+  { note: 3, duration: 1000, display: "C" },
+  { note: 4, duration: 1000, display: "D" },
+  { note: 0, duration: 1000, display: "A" },
+  { note: 0, duration: 1000, display: "G" }
 ];
 let trigger = 0;
 let autoplay = false;
@@ -39,9 +53,9 @@ function setup() {
     }
   });
 
-  // A triangle oscillator
+
   osc = new p5.TriOsc();
-  // Start silent
+
   osc.start();
   osc.amp(0);
 }
@@ -52,68 +66,67 @@ function playNote(note, duration) {
   // Fade it in
   osc.fade(0.5,0.2);
 
-  // If we sest a duration, fade it out
   if (duration) {
     setTimeout(function() {
-      osc.fade(0,0.2);
+      osc.fade(0,0.5);
     }, duration-50);
   }
 }
 
 function draw() {
-
-  // If we are autoplaying and it's time for the next note
+  r=random(255);
+  g=random(255);
+  b=random(255);
   if (autoplay && millis() > trigger){
     playNote(notes[song[index].note], song[index].duration);
     trigger = millis() + song[index].duration;
-    // Move to the next note
+  
     index ++;
-  // We're at the end, stop autoplaying.
+
   } else if (index >= song.length) {
     autoplay = false;
   }
 
 
-  // Draw a keyboard
 
-  // The width for each key
+
+  
   let w = width / notes.length;
   for (let i = 0; i < notes.length; i++) {
     let x = i * w;
-    // If the mouse is over the key
+ 
     if (mouseX > x && mouseX < x + w && mouseY < height) {
-      // If we're clicking
+      
       if (mouseIsPressed) {
-        fill(100,255,200);
-      // Or just rolling over
+        fill(r,g,b);
+      
       } else {
-        fill(127);
+        fill(100);
       }
     } else {
       fill(200);
     }
 
-    // Or if we're playing the song, let's highlight it too
     if (autoplay && i === song[index-1].note) {
-      fill(100,255,200);
+      fill(r,g,b);
     }
 
-    // Draw the key
+
     rect(x, 0, w-1, height-1);
   }
 
 }
 
-// When we click
+
 function mousePressed(event) {
   if(event.button == 0 && event.clientX < width && event.clientY < height) {
-    // Map mouse to the key index
+    
     let key = floor(map(mouseX, 0, width, 0, notes.length));
     playNote(notes[key]);
   }
 }
 
-// Fade it out when we release
+
 function mouseReleased() {
-  osc.fade(0,0.5);
+  osc.fade(0,2);
 }
